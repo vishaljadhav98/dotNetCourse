40ec029765c4121a30d49b2c13bd38ac414a366a
Vishal <vishaljagannath547@gmail.com>
2024-09-27 12:06:32 +0530

    Add project files.

---
+++ Assignments/SingleInheritance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    public class Person2
+    {
+        public int rollNo;
+        public string name;
+    }
+    public class Student : Person2
+    {
+        public Student(int rollNo, string name)
+        {
+            this.rollNo = rollNo;
+            this.name = name;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Roll No. : " + rollNo);
+            Console.WriteLine("Name : " + name);
+        }
+    }
+    internal class SingleInheritance
+    {
+        public static void Main(string[] arg)
+        {
+            Student emp = new Student(101, "Vishal");
+            emp.Display();
+        }
+    }
+}
