f8de7df05eee221e33d9e6fceb2d68644224e492
Vishal <vishaljagannath547@gmail.com>
2024-09-27 12:27:19 +0530

    Assignment Hirarchical Inheritance

---
+++ Assignments/OOPs/HirarchicalInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    public class Shape
+    {
+        public string color;
+        public void DisplayDetails()
+        {
+            Console.WriteLine(color);
+        }
+    }
+    public class Circle2 : Shape
+    {
+        public Circle2(string color)
+        {
+            this.color = color;
+        }
+    }
+    public class Rectangle2 : Shape
+    {
+        public Rectangle2(string color)
+        {
+            this.color = color;
+        }
+    }
+    internal class HirarchicalInheritance
+    {
+        public static void Main(string[] args)
+        {
+            Circle2 circle = new Circle2("White");
+            Console.Write("Circle color : ");
+            circle.DisplayDetails();
+            Rectangle2 rectangle2 = new Rectangle2("Black");
+            Console.Write("Rectangle color : ");
+            rectangle2.DisplayDetails();
+        }
+    }
+}
