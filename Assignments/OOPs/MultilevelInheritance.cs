b51b20807d09059d11b621f5e5331878b08300f6
Vishal <vishaljagannath547@gmail.com>
2024-09-27 12:19:26 +0530

    Assignment Multilevel Inheritance

---
+++ Assignments/OOPs/MultilevelInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    public class Person3
+    {
+        public string name;
+        public string address;
+    }
+    public class Teacher : Person3
+    {
+        public int id;
+        public int salary;
+    }
+    public class Principle : Teacher
+    {
+        public Principle(int id, string name, int salary, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+            this.address = address;
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID : " + id);
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Salary : " + salary);
+            Console.WriteLine("Address : " + address);
+        }
+    }
+    internal class MultilevelInheritance
+    {
+        static void Main(string[] args)
+        {
+                Principle principle = new Principle(111, "Vishal", 100000, "Pune");
+                principle.Display();
+        }
+    }
+}
